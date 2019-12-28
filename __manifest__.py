55250fe768e105ed411cc867fcad2b5510686ef3
shaheen hossain <rapidgrps@gmail.com>
2019-12-28 21:25:30 +0600

    remove the enterprise label

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Remove Enterprise',
+    'summary': 'Remove enterprise modules and setting items',
+    'version': '12.0.1.1.0',
+    'category': 'Maintenance',
+    'author': 'Eagle Corporte edition',
+    'website': 'https://eagle-erp.com/',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'data/ir_module_module.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
